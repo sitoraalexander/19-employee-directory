65d2a4d497c2e87f8d833497895acca30b16ba8d
Sitora Alexander <sitoraalexander@gmail.com>
2021-01-17 21:08:59 -0500

    table compotenets optimized

---
+++ src/components/Header/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./style.css";
+
+function Header () {
+    return (
+        <div className="jumbotron jumbotron-fluid">
+            <div>
+                <h1>Employee Directory</h1>
+            </div>
+        </div>
+    )
+}
+
+export default Header;
